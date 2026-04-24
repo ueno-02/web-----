73019a19f72fb40daa5f05973d60cb09e0a719cb
HP016\HP016 <ue2768308@gmail.com>
2026-04-24 13:17:35 +0900

    中身を記述

---
+++ renshuu/test.php
@@ -0,0 +1,47 @@
+<?php 
+$Hello = 'Hello,World!';
+
+function title($word) {
+    echo '<h1 style=" color:red">' . $word . '</h1>';
+}
+
+title($Hello);
+
+title("こんにちは");
+?>
+
+<ul>
+    <li>ハンバーガー</li>
+    <li>チーズバーガー</li>
+    <li>フィレオフィッシュ</li>
+</ul>
+
+<?php
+
+$burgur = [
+    'ハンバーガー',
+    'チーズバーガー',
+    'フィレオフィッシュ'
+];
+
+function menu($items) {
+    echo '<ul>';
+    foreach($items as $item){
+        echo '<li style="color:red">' . $item . '</li>';
+    }
+    echo '</ul>';
+}
+
+menu($burgur);
+
+$drinks = [
+    'コーラ',
+    'オレンジジュース',
+    'お茶',
+    'シェイク',
+    'ジンジャーエール'
+];
+
+menu($drinks);
+
+?>
